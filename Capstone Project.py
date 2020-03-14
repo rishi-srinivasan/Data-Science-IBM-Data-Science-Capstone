f937d26e53c03fbb3ff908459c26086b6eae4f1c
rishi-srinivasan <62171004+rishi-srinivasan@users.noreply.github.com>
2020-03-14 15:11:05 +0530

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This Notebook is used for Capstone Project - IBM Data Science
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
